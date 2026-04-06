87c3901c73ed1fcac583a24fd09eb7effc4704da
torres-jp <torres.jpw@gmail.com>
2026-04-06 20:12:50 -0300

    Add examples of print function usage in 01_print.py

--- 01_basics/01_print.py
+++ 01_basics/01_print.py
@@ -5,3 +5,11 @@
 
 
 print('hello world')
+print("Comillas dobles")
+
+print('python', 'es', 'un', 'lenguaje', 'de', 'programacion') # Podemos imprimir varias cosas a la vez, separandolas por comas.
+
+print('python', 'es', 'un', 'lenguaje', 'de', 'programacion', 'friendly', sep='-') # Podemos cambiar el separador entre las cosas que imprimimos, usando el parametro sep. En este caso, el separador es un guion.
+
+print('python', 'es', 'un',end='\n')
+print('lenguaje', 'de', 'programacion') # Podemos cambiar el final de la impresion, usando el parametro end. En este caso, el final es un signo de exclamacion.
