5ad93c267d5d0c7c04ae5c11052a1b8c76fda673
torres-jp <torres.jpw@gmail.com>
2026-04-07 23:51:06 -0300

    Add casting examples and boolean evaluations in 03_cast.py; update README.md

--- 01_basics/03_cast.py
+++ 01_basics/03_cast.py
@@ -7,3 +7,22 @@
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
+print('Casting en Python: ')
+
+# print(type("100"))  # <class 'str'>
+# print(type(int("100")))
+# print(type(2.5 + int("5"))) # <class 'int'>
+
+print(int(2.5))  # 2
+# print(round(2.5))  # 2 Redondea al número par más cercano
+# print(round(3.5))  # 4 Redondea al número par más cercano
+# print(round(3.2))  # 3 Redondea al número par más cercano
+# print(round(2.6))  # 3 Redondea al número par más cercano
+
+# print(bool(3))  # True
+# print(bool(0))  # False
+# print(bool(-1))  # True
+
+# print(bool(""))  # False
+# print(bool(" "))  # True
+# print(bool("False"))  # True
