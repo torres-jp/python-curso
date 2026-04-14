8a98ac78c54373a648c175cbb2a1f19bfe44cff1
torres-jp <torres.jpw@gmail.com>
2026-04-14 16:01:18 -0300

    loop while

--- 03_loops/01_loop_while.py
+++ 03_loops/01_loop_while.py
@@ -7,9 +7,58 @@ import os
 os.system("cls")  # Limpiar la consola (en Windows, usa "cls" en lugar de "clear")
 
 print("\n Bucle while: ")
-
 # bucle con una simple condicion
-contador = 1
-while contador < 6:
-    print("Contador:", contador)
-    contador += 1  # Incrementamos el contador para evitar un bucle infinito
+# contador = 0
+# while contador <= 5:
+#     print("Contador:", contador)
+#     contador += 1  # Incrementamos el contador para evitar un bucle infinito
+
+# utilzando break para salir del bucle
+print("\n Bucle while con break: ")
+
+# contador = 0
+# while contador <= 100:
+#     contador += 1
+#     print(contador)
+#     if contador % 5 == 0:
+#         print("Contador es múltiplo de 5, saliendo del bucle.")
+#         break  # Salir del bucle cuando el contador sea múltiplo de 5
+
+
+# utilizando continue para saltar una iteracion
+print("\n Bucle while con continue: ")
+# contador = 0
+# while contador < 10:
+#     contador += 1
+#     if contador % 2 == 0:
+#         continue
+#     print(contador)
+
+
+# else
+print("\n Bucle while con else: ")
+# contador = 0
+# while contador < 5:
+#     print(contador)
+#     contador += 1
+# else:
+#     print("Bucle while finalizado")
+
+
+# contador = 0
+# while contador < 5:
+#     print(contador)
+#     contador += 1
+#     break
+# else:
+#     print("Bucle while finalizado")
+
+
+# pedirle al usuario un numero que sea positivo
+numero = -1
+while numero < 0:
+    numero = int(input("Escriba un numero positivo: "))
+    if numero < 0:
+        print("El numero debe ser positivo.")
+
+print(f"El numero que escribiste es: {numero}")
