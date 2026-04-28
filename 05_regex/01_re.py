cbd43749ae3cd6f416ded1a3496c2023b7699920
torres-jp <torres.jpw@gmail.com>
2026-04-28 01:58:38 -0300

    regex_1

--- 05_regex/01_re.py
+++ 05_regex/01_re.py
@@ -3,7 +3,10 @@
 ###
 
 # 1. Importar el modulo de expresiones regulares (re)
+import os
 import re
+
+os.system("cls")
 
 """Las expresioens regulares son una  secuencia de  caracteres que forman un patron de  busqueda.
 Se utilizan para la busqueda de cadenas de texto , validacion de datos, etc...
@@ -39,3 +42,66 @@ print(result.start())
 
 # .end() devuelve la posicion final de la coincidiencia
 print(result.end())
+
+
+###########
+###########
+
+# EJERCICIO 01
+# Encuentra la primera ocurrencia de la palabra IA en el siguiente texto
+# e indica en que posicion empieza y termina la coincidencia
+text = "Todo el mundo dice que la IA nos va quitar el trabajo. Pero solo hace falta ver como puede cargar con las Regex para ir con cuidado"
+pattern = "IA"
+found_ia = re.search(pattern, text)
+
+if found_ia:
+    print(
+        f"He encontrado el patron en el texto en la posicion {found_ia.start()} y termina en la posicion {found_ia.end()}"
+    )
+else:
+    print("No he encontrado el patron en el texto")
+
+
+# -------------------------
+
+### Encontrar todas las coincidencias de un patron
+# .findall() devuelve una lista con todas las coincidencias
+
+text = "Me gusta Python. Python es lo maximo, aunque Python no es tan dificil."
+pattern = "Python"
+
+matches = re.findall(pattern, text)
+print(len(matches))
+
+
+# -----------------------
+
+# .iter() devuelve un iterador que contiene todos los resultados de la busqueda.
+
+text = "Me gusta Python. Python es lo maximo, aunque Python no es tan dificil."
+pattern = "Python"
+
+matches = re.finditer(pattern, text)
+
+for match in matches:
+    print(match.group(), match.start(), match.end())
+
+
+### Modificadores
+# Los modificadores son opciones que se pueden agregar a un patron para cambiar su comportamiento
+
+# re.IGNORECASE: Ignora las mayusculas y minusculas
+
+text = "Todo el mundo dice que la IA nos va quitar el trabajo. Pero solo hace falta ver como puede cargar con las Regex para ir con cuidado, pero la ia no es tan mala. Aprende a usar Ia"
+pattern = "IA"
+found_ia = re.findall(pattern, text, re.IGNORECASE)
+
+if found_ia:
+    print(found_ia)
+
+
+### Reemplazar el texto
+# .sub() reemplaza todas las coincidencias del patron en un texto
+text = "hola mundo, hola de nuevo!!"
+pattern = "hola"
+replacement = "Adios"
