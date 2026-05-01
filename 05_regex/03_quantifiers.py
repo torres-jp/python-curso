6ff0c3d74eab13ae1333a8b0b8eeec6784209298
torres-jp <torres.jpw@gmail.com>
2026-04-30 21:37:56 -0300

    quantifiers exercises

--- 05_regex/03_quantifiers.py
+++ 05_regex/03_quantifiers.py
@@ -4,3 +4,32 @@
 ###
 
 import re
+import os
+
+os.system("cls")
+# *: Puede aparecer 0 o mas veces
+text = "aaaba"
+pattern = "a*"
+matches = re.findall(pattern, text)
+# print(matches)
+
+# Ejercicio 1:
+# ¿Cuantas palabras tiene de a 0 o mas 'a' y despues una b?
+
+# +: Una a mas veces
+text = "dddd aaa ccc bb casa asado"
+pattern = "a+"
+matches = re.findall(pattern, text)
+# print(matches)
+
+# ?: Puede aparecer 0 o 1 vez)
+text = "aaaba"
+pattern = "a?b"
+matches = re.findall(pattern, text)
+print(matches)
+
+# {n} Exactamente n veces
+text = "aaaaaa"
+pattern = "a{3}"
+matches = re.findall(pattern, text)
+# print(matches)
