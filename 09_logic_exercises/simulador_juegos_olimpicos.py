971da427f909336f89d1999ce16b2f7be2fa7276
torres-jp <torres.jpw@gmail.com>
2026-06-12 06:05:23 -0300

    add new project

---
+++ 09_logic_exercises/simulador_juegos_olimpicos.py
@@ -0,0 +1,12 @@
+print("Simulador de Juegos Olímpicos")
+
+print("")
+
+while True:
+    print("1. Registrar eventos")
+    print("2. Registro de participantes")
+    print("3. Simulacion de eventos")
+    print("4. Creacion de infores")
+    print("5. Salir")
+
+    opcion = input("Seleccione una opcion: ")
