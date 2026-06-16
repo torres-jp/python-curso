14961a88145ea4cf2cb0d7f984f90132081fe8ec
torres-jp <torres.jpw@gmail.com>
2026-06-16 01:28:12 -0300

    feat: add Olympic Games simulator with event registration options

--- 10_snake_game/snake.py
+++ 10_snake_game/snake.py
@@ -17,7 +17,14 @@ class Snake:
 
 
 class Food:
-    pass
+    def __init__(self):
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+
+        self.coordinates = [x, y]
+        canvas.create_oval(
+            x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food"
+        )
 
 
 def next_turn():
